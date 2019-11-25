0cbec25f009cd08931421ffe2ea43b586dfd2d12
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-11-25 15:21:56 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function check() {
+    //tetret
+    var saldo = document.getElementById("saldo").value;
+    if (saldo=500) {
+        document.getElementById("tekst").innerHTML = "salaris is gestort"
+    } else {
+        document.getElementById("tekst").innerHTML = "salaris met bonus is gestort";
+    }
+}
