e932ba0bee1223b4906f41e65c576afd7fc5b8d6
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-11-25 15:11:36 +0100

    opdracht 2

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,3 +1,10 @@
-//javascript code
-
-tetestst
+//javascript
+function check() {
+    //tetret
+    var saldo = document.getElementById("saldo").value;
+    if (saldo<25) {
+        document.getElementById("tekst").innerHTML = "uw saldo is te laag"
+    } else {
+        document.getElementById("tekst").innerHTML = "saldo is genoeg";
+    }
+}
