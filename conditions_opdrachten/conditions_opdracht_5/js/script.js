46e7046d47b5bcf77f0accdf036394d6f8ff4562
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-11-26 14:53:19 +0100

    commit

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+
+function bereken() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    var getal1 = document.getElementById("operator").value;
+
+    if (operator == + ) {
+        document.getElementById("antwoord").innerHTML = getal1 + getal2;
+
+        
+    }
+    
+    if (saldo == 500) {
+        
+    } else if { (saldo > 500) {
+
+    } else {
+        
+    }
+}
