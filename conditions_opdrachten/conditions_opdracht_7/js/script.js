d3bc046d12850a01365b73fecd9f671acc2a8dae
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-12-03 13:31:58 +0100

    opdracht 7

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+function check(){
+    var saldo = document.getElementById("saldo").value;
+
+    if(nummer==500) {
+        document.getElementById("tekst").style.backgroundColor = "green"
+    } else if(nummer>=500) {
+        document.getElementById("tekst").style.backgroundColor = "Blue"
+    }
+    else{
+        document.getElementById1("tekst").style.backgroundColor = "Black"
+    }
+}
