eac7a34bd229b19b20031830282886ef2f06fa53
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-12-09 14:45:32 +0100

    loooops

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+var i;
+var tekst=""';
+for (let i = 1; i <=50; i++)
+    if (i==20 || i==50) {
+        tekst += "dit is mooi getal<br>";
+        
+    } else {
+        tekst += i + "<br>";
+    }
+        console.log(i);
+}
+
+document.getElementById("uitvoer").innerHTML=tekst;
